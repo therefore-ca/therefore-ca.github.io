00724cbbcae679d9726b26e606b32714e0b0adf2
Sean <sean@therefore.ca>
2015-05-26 15:42:43 -0400

    mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-$.material.init();
+$.material.init(),$(document).ready(function(){"use strict";var a={menuOpen:function(){console.log("menu-open")},menuClose:function(){console.log("menu-close")}};$("a.menu-open").click(function(a){a.preventDefault(),$("a.menu-open").addClass("mobile-hidden"),$(".menu-container").removeClass("mobile-hidden closed").addClass("open")}),$("a.menu-close").click(function(b){b.preventDefault(),$(".menu-container").addClass("mobile-hidden open").addClass("closed"),$("a.menu-open").removeClass("mobile-hidden"),a.menuClose()})});
